7c8e4888265d4343c6cb0859dcf046ff3ad37a41
sunqingjiang <aiwozhe@yeah.net>
2020-01-20 10:02:34 +0800

    更新包的描述信息使用json字符串

--- sql/codepush-v0.5.1-patch.sql
+++ sql/codepush-v0.5.1-patch.sql
@@ -2,6 +2,6 @@ ALTER TABLE `deployments_versions`
 ADD COLUMN `label_id` int(11) UNSIGNED NOT NULL DEFAULT 0 AFTER `current_package_id`;
 
 ALTER TABLE `packages`
-MODIFY COLUMN `description` json NOT NULL AFTER `deployment_id`;
+MODIFY COLUMN `description` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `deployment_id`;
 
 UPDATE `versions` SET `version` = '0.5.1' WHERE `type` = '1';
